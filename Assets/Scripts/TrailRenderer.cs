874363f7e9d0820ed40ee9221d05c15e1d4235e5
Zerosin <alstn1339@gmail.com>
2023-12-04 05:35:58 +0900

    20231204 ball trail

---
+++ Assets/Scripts/TrailRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TrailRenderer : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private List<Vector3> points = new List<Vector3>();
+    private List<float> pointTimes = new List<float>(); // 각 포인트의 생성 시간을 추적
+    private float trailLifeTime = 1.0f; // 궤적이 지속되는 시간 (1초)
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position; // 현재 공의 위치
+
+        // 공의 위치가 변경되었는지 확인
+        if (points.Count == 0 || points[points.Count - 1] != currentPosition) {
+            points.Add(currentPosition);
+            pointTimes.Add(Time.time); // 현재 시간 추가
+        }
+
+        // 오래된 궤적 포인트 제거
+        while (points.Count > 0 && Time.time > pointTimes[0] + trailLifeTime) {
+            points.RemoveAt(0);
+            pointTimes.RemoveAt(0);
+        }
+
+        // LineRenderer 업데이트
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPositions(points.ToArray());
+    }
+}
--- README.md
+++ README.md
@@ -2,9 +2,30 @@
 
 ![lobby.gif](./Screenshot/gif_lobby.gif)</br>
 ![main.gif](./Screenshot/gif_main.gif)</br>
+![trail.gif](./Screenshot/gif_trail.gif)</br>
 
-## 현진님 브랜치 적용</br>
+## 1. 현진님 브랜치 적용</br>
 ![main](./Screenshot/20231204_main.png)</br>
 
-## 로비 UI</br>
+동영상과 연동한 결과입니다.(웹캠x)</br>
+UI와 카메라 각도를 약간 수정하였습니다.</br>
+
+## 2. 공 궤도 기능 추가</br>
+![trail](./Screenshot/20231204_trail.png)</br>
+사용된 스크립트
+- TrailRenderer.cs (공 오브젝트에 부착)
+
+기타
+- TennisTrail(Materials)
+
+공이 날라간 궤도를 따라서 선이 생성됩니다.</br>
+궤도는 생성 후 1초 뒤에 사라집니다.(스크립트로 조절 가능)</br>
+궤도의 색깔은 Materials을 이용해 변경 가능합니다.</br>
+
+## 3. 로비 UI (유니티 연습)</br>
 ![lobby](./Screenshot/20231204_lobby.png)</br>
+사용된 스크립트
+- LobbyManager.cs
+
+3D 테니스 채가 돌아갑니다.</br>
+버튼을 누르면 다음 씬으로 이동합니다.</br>
