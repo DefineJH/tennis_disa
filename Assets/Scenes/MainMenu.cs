eec092be009eb9a72b9a6418d6de096ac747fcfb
hoyoon <officialhoyoon@gmail.com>
2023-12-05 00:03:02 +0900

    main menu ui

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Praticemode()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void mirrormode()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/practice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class practice : MonoBehaviour
+{
+    public void Forehand()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+    public void Volley()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+    public void Smash()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
